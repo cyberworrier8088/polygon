c47ebc38723845a22af43b29cdf86aa559638536
cyberworrier8088 <cyberworrier39@gmail.com>
2026-06-26 07:36:03 +0530

    i added the donut and star in my page

--- main.js
+++ main.js
@@ -14,8 +14,15 @@ const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+// adding donut shape
+const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const donut_tex = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const donut = new THREE.Mesh(donut_geo, donut_tex);
+scene.add(donut);
+
+
 // move the camera back so we can see the cube
-camera.position.z = 5;
+camera.position.z = 18;
 
 // create an animation loop to render the scene
 function animate() {
@@ -29,3 +36,21 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+
+// adding star to page
+function add_star() {
+  const star_geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const star_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(star_geometry, star_material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(900).fill().forEach(add_star);
+
